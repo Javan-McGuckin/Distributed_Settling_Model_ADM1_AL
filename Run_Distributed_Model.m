8dcd9ae76bda1b9062dc47f25020655ed8d506e4
Javan-McGuckin <116236156+Javan-McGuckin@users.noreply.github.com>
2022-12-02 10:39:52 +1000

    Adding Files to GIT

---
+++ Run_Distributed_Model.m
@@ -0,0 +1,13 @@
+%% Parameter Definition
+
+% Define Split Ratios
+split_1_1 = 0.5;
+split_1_2 = 0.5;
+split_1_3 = 0.5;
+split_2_1 = 0.5;
+split_2_2 = 0.5;
+split_2_3 = 0.5;
+split_3_1 = 0.5;
+split_3_2 = 0.5;
+split_3_3 = 0.5;
+
